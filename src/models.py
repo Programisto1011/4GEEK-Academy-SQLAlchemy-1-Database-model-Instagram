720e4d986f21c5318df3c27f29de65bccc27f94b
Programisto1011 <victormaynougomez@gmail.com>
2022-02-11 10:54:12 +0000

    Fix relationship followers

--- src/models.py
+++ src/models.py
@@ -1,36 +1,33 @@
 #Draw diagram of model: python src/models.py
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'Follower'
-    user_from_id = Column(Integer, ForeignKey('User.ID')),
-    # user_to_id = Column(Integer, ForeignKey('User.ID')),
-
-    def __repr__(self):
-        return '<Follower %r>' % self.user_from_id
-
-    def serialize(self):
-        return {
-            "user_from_id": self.user_from_id,
-            "user_to_id": self.user_to_id
-        }
+followers = Table('followers', Base.metadata,
+    Column('follower_id', ForeignKey('user.ID')),
+    Column('followed_id', ForeignKey('user.ID'))
+)
 
 class User(Base):
-    __tablename__ = 'User'
-    ID = Column(Integer, primary_key=True),
-    username = Column(String(80), unique=True, nullable=False),
-    firstname = Column(String(80), nullable=False),
-    lastname = Column(String(80), nullable=False),
-    email = Column(String(80), unique=True, nullable=False),
-    child_1 = relationship('Follower', backref="user"),
+    __tablename__ = 'user'
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    firstname = Column(String(80), nullable=False)
+    lastname = Column(String(80), nullable=False)
+    email = Column(String(80), unique=True, nullable=False)
+    followed = relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == ID),
+        secondaryjoin=(followers.c.followed_id == ID),
+        backref=backref('followers', lazy='dynamic'),
+        lazy='dynamic')
+
 
     def __repr__(self):
         return '<User %r>' % self.username
